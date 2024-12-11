54f859d83c02ec8d522f9d85dbed4b9d3118321a
Marley <jeimymorales234@gmail.com>
2024-12-10 22:11:46 -0500

    Nuevo main

---
+++ 4. Proyecto/4. Proyecto/Proyecto/DAL/RepositoryFactory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class RepositoryFactory
+    {
+        public static IRepository CreateRepository()
+        {
+            //return new EFRepository(new Entities.Sales_DB_PruebaEntities());
+            var Context = new Entities.Sales_DB_PruebaEntities();
+            Context.Configuration.ProxyCreationEnabled = false;
+            return new EFRepository(Context);
+        }
+    }
+}
---
+++ 4. Proyecto/4. Proyecto/Proyecto/Service/EmailService.cs
@@ -0,0 +1,37 @@
+﻿using Common.Interfaces;
+using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
+
+namespace Service
+{
+    public class EmailService : IEmailService
+    {
+        private const string SmtpServer = "smtp.gmail.com";
+        private const int SmtpPort = 587;
+        private const string SenderEmail = "jeimymorales234@gmail.com";
+        private const string SenderPassword = "wwxxktzzhyszqywj";
+        private const string SenderName = "Gestión de productos";
+
+        public async Task SendEmailAsync(string recipientEmail, string subject, string body)
+        {
+            using (var smtp = new SmtpClient(SmtpServer, SmtpPort))
+            {
+                smtp.Credentials = new NetworkCredential(SenderEmail, SenderPassword);
+                smtp.EnableSsl = true;
+
+                var mail = new MailMessage
+                {
+                    From = new MailAddress(SenderEmail, SenderName),
+                    Subject = subject,
+                    Body = body,
+                    IsBodyHtml = true
+                };
+
+                mail.To.Add(recipientEmail);
+
+                await smtp.SendMailAsync(mail);
+            }
+        }
+    }
+}
