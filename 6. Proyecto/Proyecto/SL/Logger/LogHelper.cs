b555ea37935a9bcf5d11f2e2c987e6cf1540bd2d
ErickRamirezO <87491977+ErickRamirezO@users.noreply.github.com>
2024-12-11 11:54:41 -0500

    Changes for implement Logger

---
+++ 6. Proyecto/Proyecto/SL/Logger/LogHelper.cs
@@ -0,0 +1,37 @@
+﻿using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SL.Logger
+{
+    public class LogHelper
+    {
+        static LogHelper()
+        {
+            // Configurar Serilog para escribir en la consola
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.Console() // Este es el sink para la consola
+                .WriteTo.File("logs/app.log", rollingInterval: RollingInterval.Day) // Este es el sink para archivo
+                .CreateLogger();
+        }
+
+        public static void LogInformation(string message)
+        {
+            Log.Information(message);
+        }
+
+        public static void LogWarning(string message)
+        {
+            Log.Warning(message);
+        }
+
+        public static void LogError(string message, Exception ex)
+        {
+            Log.Error(ex, message);
+        }
+    }
+
+}
