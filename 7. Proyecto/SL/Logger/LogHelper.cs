ae9fc3512d33881fff884104a20c7e47b13888da
ErickRamirezO <epramirez2@espe.edu.ec>
2024-12-12 19:04:58 -0500

    Inclusión de función para escape de caracteres especiales en formularios y actualización del Logger

--- 7. Proyecto/SL/Logger/LogHelper.cs
+++ 7. Proyecto/SL/Logger/LogHelper.cs
@@ -1,9 +1,6 @@
 ﻿using Serilog;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 
 namespace SL.Logger
 {
@@ -11,11 +8,27 @@ namespace SL.Logger
     {
         static LogHelper()
         {
-            // Configurar Serilog para escribir en la consola
+            Serilog.Debugging.SelfLog.Enable(msg => System.Diagnostics.Debug.WriteLine(msg));
+
+            // Ruta específica para los logs
+            var logDirectory = @"C:\Logs";
+
+            // Crear el directorio si no existe
+            if (!Directory.Exists(logDirectory))
+            {
+                Directory.CreateDirectory(logDirectory);
+            }
+
+            var logFilePath = Path.Combine(logDirectory, "app.log");
+
+            // Configurar Serilog para escribir en la consola y en un archivo en la ruta especificada
             Log.Logger = new LoggerConfiguration()
                 .WriteTo.Console() // Este es el sink para la consola
-                .WriteTo.File("logs/app.log", rollingInterval: RollingInterval.Day) // Este es el sink para archivo
+                .WriteTo.File(logFilePath, rollingInterval: RollingInterval.Day) // Este es el sink para archivo
                 .CreateLogger();
+
+            // Log de la ruta del archivo de log
+            Log.Information($"El archivo de log se creará en: {logFilePath}");
         }
 
         public static void LogInformation(string message)
@@ -33,5 +46,4 @@ namespace SL.Logger
             Log.Error(ex, message);
         }
     }
-
 }
